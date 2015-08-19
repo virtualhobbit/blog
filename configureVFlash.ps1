e88b831a042d0e0ba3ff61973d960bfff8d67b9e
virtualhobbit <virtualhobbit@users.noreply.github.com>
2015-08-19 14:47:11 +0200

    Wednesday Tidbit: Using PowerCLI extensions fling to configure vFlash

---
+++ configureVFlash.ps1
@@ -0,0 +1,33 @@
+# Variables
+
+$vc = "vc2.uk.mdb-lab.com"
+$cred = Get-Credential
+$cluster = "London Cluster"
+
+# Connect to vCenter
+Connect-VIServer -Server $vc -Credential $cred
+
+Import-Module VMware.VimAutomation.Extensions
+
+$myCluster = Get-Cluster $cluster
+ForEach ($esxi in ($myCluster | Get-VMHost)) {
+ 
+# Read hosts in hosts.txt   
+Write-Host "Getting current vFRC configuration for" $esxi -foregroundcolor "magenta"  
+$vFlashConfig = Get-VMHostVFlashConfiguration -VMHost $esxi
+
+Write-Host "Getting" $esxi "host SSDs to be used by vFRC" -foregroundcolor "magenta"  
+$vFlashDisk = (Get-ScsiLun -VMHost $esxi -CanonicalName mpx.vmhba1:C0:T1:L0 | Get-VMHostDisk)
+
+# Enable vFRC on selected host   
+Write-Host "Setting vFRC configuration for" $esxi -foregroundcolor "magenta"
+Set-VMHostVFlashConfiguration -VFlashConfiguration $vFlashConfig -AddDevice $vFlashDisk
+
+# Set vFRC host swap cache
+Write-Host "Setting vFRC host swap cache for" $esxi -foregroundcolor "magenta"
+Set-VMHostVFlashConfiguration -VFlashConfiguration $vFlashConfig -SwapCacheReservationGB 150
+
+}
+ 
+# Disconnect from vCenter
+Disconnect-VIServer $vc -confirm:$false
